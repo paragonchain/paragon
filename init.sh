f3804f2f942f4548d23f430134753856c56eb0e6
MichaelFioretti <michael@hijro.com>
2018-05-11 22:08:44 -0400

    Update scripts

--- init.sh
+++ init.sh
@@ -5,5 +5,6 @@ git clone https://github.com/ethereum/go-ethereum.git
 cd go-ethereum
 git checkout tags/v1.8.7
 make geth && cd ..
+geth --networkid="7755" --datadir chain init chain/genesis.json
 cp static-nodes.json chain/geth/static-nodes.json
 echo "Setup complete. Please run './start.sh' to start the node"
