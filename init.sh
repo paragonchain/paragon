4063d6245cd0548ed5cd6d5f0ec676148ef80817
MichaelFioretti <michael@hijro.com>
2018-05-11 21:56:34 -0400

    Add static nodes file and update init script

--- init.sh
+++ init.sh
@@ -4,5 +4,6 @@
 git clone https://github.com/ethereum/go-ethereum.git
 cd go-ethereum
 git checkout tags/v1.8.7
-make geth
-cd .. && echo "Setup complete. Please run './start.sh' to start the node"
+make geth && cd ..
+cp static-nodes.json chain/geth/static-nodes.json
+echo "Setup complete. Please run './start.sh' to start the node"
